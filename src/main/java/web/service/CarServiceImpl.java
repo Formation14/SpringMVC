5115dce76c72709944a9729814755c1dcb8cd728
pavel <paveltis@tut.by>
2021-08-19 10:37:09 +0300

    Initial commit

---
+++ src/main/java/web/service/CarServiceImpl.java
@@ -0,0 +1,29 @@
+package web.service;
+
+import org.springframework.stereotype.Service;
+import web.model.Car;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Service
+public class CarServiceImpl implements CarService {
+
+    private static final List<Car> cars = Arrays.asList(
+            new Car("W205", "Mercedes", 2500000),
+            new Car("X5", "BMW", 3000000),
+            new Car("Q7", "Audi", 825000),
+            new Car("CX-5", "Mazda", 1500000),
+            new Car("LX III", "Lexus", 2900000));
+
+    @Override
+    public List<Car> printSomeCars(Integer count) {
+        if (count == null) {
+            return cars;
+        }
+        if (count < 5) {
+            return cars.subList(0, count);
+        }
+        return cars;
+    }
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,6 @@
+<html>
+<body>
+<h2>Hello World!</h2>
+<p><a href="/cars">Car list</a></p>
+</body>
+</html>
